4dc896c15538899421316cf5240f23b4ca62b253
rrameshp <rrameshp@gmail.com>
2015-12-03 14:22:08 +0530

    First Commit

---
+++ Boost_Test.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : Boost_Test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/lambda/bind.hpp>
+
+int main()
+{
+	using namespace boost::filesystem;
+	using namespace boost::lambda;
+
+	path the_path( "/home/Work/EBooks" );
+
+	int cnt = std::count_if (
+			directory_iterator(the_path),
+			directory_iterator(),
+			bind( static_cast<bool(*)(const path&)>(is_regular_file),
+			bind ( &directory_entry::path, _1) ) );
+
+	std::cout << cnt << std::endl;
+	return 0;
+}
